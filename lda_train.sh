597e511e41f02c38e32b8343e70b9b898b5f83c6
Adam Anderson <84adam@users.noreply.github.com>
2019-10-04 15:56:11 -0700

    Create lda_train.sh

---
+++ lda_train.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+IMAGE_NAME=${1:-train_model}
+
+SHARED_PATH="$HOME/lda_model"
+
+# HYPERPARAMETERS
+
+NO_BELOW=${2:-20}
+NO_ABOVE=${3:-0.85}
+KEEP_N=${4:-100000}
+TOPICS=${5:-10}
+PASSES=${6:-2}
+WORKERS=${7:-2}
+DOCS=${8:-processed_docs.pkl}
+
+mkdir -p $SHARED_PATH;
+
+sudo docker run -it -v $SHARED_PATH:/root $IMAGE_NAME $NO_BELOW $NO_ABOVE $KEEP_N $TOPICS $PASSES $WORKERS $DOCS;
+
+echo "Model training complete. Models saved to $SHARED_PATH.";
