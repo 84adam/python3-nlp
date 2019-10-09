f070fc48e549c09e3a898dfbc5cc4e143e0366e3
Adam Anderson <84adam@users.noreply.github.com>
2019-10-09 12:45:58 +0000

    Update tar_model.sh

--- tar_model.sh
+++ tar_model.sh
@@ -9,8 +9,7 @@ if [ -f "models/saved-model.tar.gz" ];
 	then mv models/saved-model.tar.gz models/saved_model_$DT.tar.gz;
 fi
 
-mv -f ./tf-lda* models/saved-model/;
+cp ./tf-lda* models/saved-model/;
 
 tar czf models/saved-model.tar.gz models/saved-model/;
 chmod 666 models/saved-model.tar.gz;
-cp models/saved-model.tar.gz $HOME/
