1f1b1b7bf131d551720606173f078d98eaf938dd
Adam Anderson <84adam@users.noreply.github.com>
2019-11-19 13:25:27 -0800

    add gensim evaluation metric: coherence='c_v'

--- evaluate.py
+++ evaluate.py
@@ -19,24 +19,34 @@ def load_model():
   return model, dictionary
 
 def load_corpus():
+  # where processed_docs.pkl contains the lemmatized/tokenized version of each document in the corpus
   processed_docs = pd.read_pickle('processed_docs.pkl')
   dictionary = load_model()[1]
   bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
   tfidf = models.TfidfModel(bow_corpus)
   corpus = tfidf[bow_corpus]
-  return corpus
+  return corpus, processed_docs
 
 # calculate model perplexity and coherence scores
-# for each a higher score is better
+# for perplexity and coherence:u_mass a higher score is better
 # e.g. for log perplexity: -13.5 is better than -15.5
-# e.g. for coherence: -1.5 is better than -3.5
+# e.g. for coherence (u_mass): -1.5 is better than -3.5
+# for coherence:c_v a lower score is better
+# e.g. for coherence (c_v): 0.55 is better than 0.65
+# c_v has been found to have the highest correlation with human-evaluated topic model coherence
+# see: https://svn.aksw.org/papers/2015/WSDM_Topic_Evaluation/public.pdf
 
 def perplex(lda_model, corpus):
   perplexity = lda_model.log_perplexity(corpus)
   return perplexity
 
-def cohere(lda_model, corpus, dictionary):
+def cohere_umass(lda_model, corpus, dictionary):
   coherence_model_lda = CoherenceModel(model=lda_model, corpus=corpus, dictionary=dictionary, coherence='u_mass')
+  coherence_lda = coherence_model_lda.get_coherence()
+  return coherence_lda
+
+def cohere_cv(lda_model, texts, dictionary):
+  coherence_model_lda = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
   coherence_lda = coherence_model_lda.get_coherence()
   return coherence_lda
 
@@ -44,8 +54,12 @@ if __name__ == '__main__':
   topic_model = load_model()
   lda_model = topic_model[0]
   dictionary = topic_model[1]
-  corpus = load_corpus()
+  read_corpus = load_corpus()
+  corpus = read_corpus[0]
+  processed_docs = read_corpus[1]
   p_score = perplex(lda_model, corpus)
-  c_score = cohere(lda_model, corpus, dictionary)
+  c_umass_score = cohere_umass(lda_model, corpus, dictionary)
+  c_cv_score = cohere_cv(lda_model, processed_docs, dictionary)
   print(f'Model Log Perplexity Score: {p_score}')
-  print(f'Model Coherence Score: {c_score}')
+  print(f'Model Coherence Score (u_mass): {c_umass_score}')
+  print(f'Model Coherence Score (c_v): {c_cv_score}')
