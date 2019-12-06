af1be9e4b40a7b1a8d955cf24fa3bcc97f2d2c88
Adam Anderson <84adam@users.noreply.github.com>
2019-12-06 12:37:54 -0800

    remove unused functions

--- grid_train.py
+++ grid_train.py
@@ -1,60 +1,17 @@
 import sys
 import os
-import nltk
-import spacy
 import gensim
-import sklearn
-import keras
 import pandas as pd  
 import numpy as np
 from itertools import chain
-from nltk.corpus import stopwords
-from nltk.stem import WordNetLemmatizer, SnowballStemmer
-from nltk.stem.porter import *
-nltk.download('wordnet') # run once
-nltk.download('stopwords') # run once
-from gensim.parsing.preprocessing import STOPWORDS
-from gensim.utils import simple_preprocess
 from gensim import corpora, models
 from gensim.models.coherencemodel import CoherenceModel
 from gensim.models.ldamodel import LdaModel
 from gensim.corpora.dictionary import Dictionary
-import pyLDAvis.gensim
-from keras.preprocessing.text import text_to_word_sequence
-from sklearn.feature_extraction import stop_words
 import subprocess
 import shlex
 import matplotlib.pyplot as plt
 # %matplotlib inline
-
-def default_stop():
-  # intersection of gensim, nltk, spacy, and sklearn stopword lists
-  default = ['me', 'inc', 'shan', "needn't", 'she', '‘s', 'therefore', 'find', 'down', 'thereupon', 'without', 'up', 'yourselves', 'many', 'eleven', 'full', 'de', 're', 'wherever', 'on', 'her', 'already', 'through', 'side', 'having', 'together', 't', 'take', "'m", 'therein', 'everyone', 'himself', 'whenever', 'them', "'s", 'once', 'forty', 'only', 'must', 'hereupon', 'moreover', 'my', 'very', 'say', 'whom', 'get', 'eg', 'does', 'll', 'indeed', 'everything', 'couldnt', '’m', 'not', 'each', 'using', 'do', 've', 'cant', 'if', 'various', 'throughout', 'otherwise', 'serious', 'd', 'regarding', 'mustn', 'yourself', 'noone', 'somewhere', 'twenty', 'most', 'thick', 'describe', 'however', 'fire', 'see', 'eight', 'while', 'besides', 'neither', 'well', 'us', 'below', 'is', "won't", 'might', 'mine', 'anywhere', 'weren', "'re", "n't", 'whereupon', 'becomes', 'should', 'hereafter', 'ours', 'during', 'a', 'ltd', 'con', 'isn', 'else', 'whither', 'shouldn', 'why', 'will', 'seems', 'ie', 'every', 'someone', 'bottom', 'ain', 'needn', 'then', 'thin', 'being', 'whereafter', 'via', 'never', 'same', "haven't", 'y', 'behind', 'name', 'give', 'move', 'some', 'six', 'we', 'whole', 'than', 'myself', 'our', "wasn't", 'now', 'whether', "mustn't", 'were', 'still', 'along', 'enough', 'for', 'yours', 'whereby', 'per', 'had', 'next', 'twelve', "doesn't", 'onto', 'cry', 'seeming', 'are', 'between', 'almost', 'third', 'latter', 'by', 'nevertheless', 'in', 'across', 'though', 'kg', 'somehow', 'out', 'show', 'no', 'either', 'didn', 'computer', '’ve', 'such', 'all', 'both', 'few', "weren't", 'from', '’d', 'doing', 'alone', 'nan', 'latterly', 's', 'although', 'fifteen', 'hasn', 'own', 'due', 'whereas', 'beyond', "you'd", "shouldn't", 'whose', 'who', 'n’t', 'unless', 'something', "shan't", 'other', 'also', 'they', 'make', 'three', 'been', 'found', 'whoever', 'doesn', 'first', 'made', 'ten', 'seem', '‘ll', 'of', 'your', 'at', 'the', 'where', 'further', 'has', 'former', 'their', 'or', 'four', 'so', 'wherein', 'empty', 'among', 'mill', 'be', 'hasnt', 'used', 'go', 'amongst', 'everywhere', 'fifty', "hadn't", '’ll', 'you', 'km', 'others', 'this', 'thru', 'may', 'wouldn', 'itself', "'d", 'please', 'could', 'done', 'several', 'afterwards', 'two', 'becoming', 'those', '‘ve', 'part', 'hundred', 'system', 'upon', "wouldn't", 'meanwhile', 'thus', '’s', 'herein', 'hadn', 'put', 'toward', 'hers', 'these', 'sometime', 'don', 'nine', 'have', 'won', 'least', 'thereafter', 'often', 'nobody', 'except', 'always', '’re', "you've", 'since', 'elsewhere', 'here', 'wasn', 'as', 'less', 'there', 'one', 'anyone', 'when', 'sometimes', 'its', 'formerly', 'ca', 'thence', 'm', "don't", 'rather', 'but', 'above', 'themselves', 'his', 'haven', 'what', 'too', 'aren', 'keep', "mightn't", 'top', 'he', 'anyhow', 'co', 'around', 'etc', 'about', 'nor', 'anyway', 'hence', '‘d', 'sixty', 'mostly', 'detail', 'anything', 'bill', 'much', "she's", 'ourselves', 'fify', 'that', 'last', 'theirs', 'really', 'back', 'un', 'yet', 'just', 'was', 'an', 'ma', "isn't", "you'll", "should've", 'until', 'off', 'perhaps', 'beside', 'nowhere', 'mightn', 'sincere', "'ll", "didn't", "it's", 'am', 'again', 'even', 'which', 'front', 'can', 'within', "aren't", 'him', "you're", 'and', 'namely', 'against', '‘re', "that'll", 'with', 'whence', 'five', 'amount', 'o', 'quite', 'call', 'interest', 'none', 'before', 'fill', 'how', 'it', 'ever', 'seemed', 'i', 'because', 'thereby', 'would', '‘m', 'couldn', "couldn't", 'did', "'ve", 'under', 'after', 'more', 'become', 'nothing', 'herself', 'to', 'any', 'over', 'into', "hasn't", 'hereby', 'towards', 'amoungst', 'whatever', 'became', 'n‘t', 'beforehand', 'another', 'cannot']
-  default = list(set(default))
-  return default
-
-def custom_stop():
-  default = default_stop()
-  additional_words = []
-  unstop = []
-  custom = []
-  for i in additional_words:
-    default.append(i)
-  for i in default:
-    if i not in unstop:
-      custom.append(i)
-  custom = list(set(custom))
-  return custom
-
-def minimal_stop():
-  default = default_stop()
-  unstop = []
-  minimal = []
-  for i in default:
-    if i not in unstop:
-      minimal.append(i)
-  minimal = list(set(minimal))
-  return minimal
 
 def pickle_df(df, pname):
   df.to_pickle(pname)
@@ -87,20 +44,15 @@ def build_tfidf(bow_corpus):
   corpus_tfidf = tfidf[bow_corpus]
   return corpus_tfidf
 
-# print topics
-def show_topics(n_topics, lda_model):
-  print(f'Built LDA/TF-IDF model with {n_topics} topics: \n')
-  for idx, topic in lda_model.print_topics():
-    print(f'Topic #{idx}: {topic}')
-
 # build topic model
 def train_lda(corpus, dictionary, n_workers, n_passes, n_topics):
-  # print("Building Bag-of-Words corpus...")
+  # Build Bag-of-Words corpus
   bow_corpus = build_bow(corpus, dictionary)
-  # print("Building TF-IDF corpus...")
+  # Build TF-IDF corpus
   corpus_tfidf = build_tfidf(bow_corpus)  
   n_features = len(list(dictionary.values()))
   print(f'Training model with {n_topics} topics, {n_passes} passes, and {n_features} features...')
+  # lda_model = gensim.models.LdaModel(corpus_tfidf, num_topics=n_topics, id2word=dictionary, passes=n_passes)
   lda_model = gensim.models.LdaMulticore(corpus_tfidf, num_topics=n_topics, id2word=dictionary, passes=n_passes, workers=n_workers)
   return lda_model
 
@@ -117,7 +69,6 @@ def save_model(lda_model, dictionary):
   lda_model.save(filename_model)
   dictionary.save(filename_dict)
   subprocess_cmd('bash tar_model.sh')
-  # print("Model and dictionary saved to ./models/saved-model")
 
 # evaluate topic model metrics of cohesion and log perplexity
 def perplex(lda_model, corpus):
@@ -133,37 +84,6 @@ def cohere_cv(lda_model, texts, dictionary):
   coherence_model_lda = CoherenceModel(model=lda_model, texts=texts, dictionary=dictionary, coherence='c_v')
   coherence_lda = coherence_model_lda.get_coherence()
   return coherence_lda
-
-# train and save a single model
-def train_single(corpus_filename, n_topics, no_below, no_above, n_passes, n_workers, keep_n):
-  corpus = load_corpus(corpus_filename)
-  dictionary = build_dict(corpus, no_below, no_above, keep_n)
-  lda_model = train_lda(corpus, dictionary, n_workers, n_passes, n_topics)
-  save_model(lda_model, dictionary)
-  print("Training complete.")
-
-# evaluate a single saved (pre-trained) model
-def eval_single(corpus_filename, n_topics, no_below, no_above):
-  processed_docs = pd.read_pickle(corpus_filename)
-  processed_docs = processed_docs['tokens']
-  filename_model = 'tf-lda.model'
-  model = gensim.models.LdaModel.load(filename_model)
-  filename_dict = 'tf-lda.dict'
-  dictionary = corpora.Dictionary.load(filename_dict)
-  bow_corpus = [dictionary.doc2bow(doc) for doc in processed_docs]
-  tfidf = models.TfidfModel(bow_corpus)
-  corpus_tfidf = tfidf[bow_corpus]
-  n_features = len(list(dictionary.values()))
-  print(f'Evaluating model trained with {n_topics} topics, n_passes={n_passes}, no_below={no_below}, no_above={no_above}, and {n_features} features...')
-  print("Calculating Log Perplexity...")
-  p_score = perplex(model, corpus_tfidf)
-  print("Calculating Coherence (u_mass)...")
-  c_umass_score = cohere_umass(model, corpus_tfidf, dictionary)
-  print("Calculating Coherence (c_v)...")
-  c_cv_score = cohere_cv(model, processed_docs, dictionary)
-  # RESULTS FOR THIS MODEL
-  results = (p_score, c_umass_score, c_cv_score)
-  return results
 
 # perform grid training and compare using evaluation metrics
 def grid_train(corpus_filename, n_topics, no_below, no_above, n_passes, n_workers, keep_n):
@@ -195,11 +115,6 @@ def grid_train(corpus_filename, n_topics, no_below, no_above, n_passes, n_worker
   return results
 
 if __name__ == '__main__':
-
-  # set stopwords
-  my_stopwords = default_stop()
-  # custom_stopwords = custom_stop()
-  # minimal_stopwords = minimal_stop()
 
   grid_results = []
   trained_topics = []
